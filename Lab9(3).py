a7dbae8d76971093a055a4bfc1752c6f1afd094c
Myika <myika2004@icloud.com>
2022-10-30 16:54:12 +0200

    Lab9(3)

--- Lab9(3).py
+++ Lab9(3).py
@@ -1,17 +1,22 @@
-n=input().split(' ')
-count=len(n)
-while count>10 or count<3:
-    print("Кількість сторін не може бути більше 10 і меньше 3")
-    n=input().split(' ')
-    count=len(n)
-for i in range(0, len(n), 1):
-    n[i]=int(n[i])
-abc=('A','B','C','D','E','F','G','H','I','J')
-masiv=[]
-for i in range(0, count-1, 1):
-    masiv.append(abc[i]+abc[i+1]+'='+str(n[i]))
-masiv.append(abc[count-1]+abc[0]+'='+str(n[count-1]))
-print(masiv)
-n.sort()
-print("Кількість", count)
-print("Найбільша сторона",n[len(n)-1])
+import operator
+a=int(input("Довжина AB: "))
+b=int(input("Довжина BC: "))
+c=int(input("Довжина CD: "))
+d=int(input("Довжина DE: "))
+e=int(input("Довжина EF: "))
+f=int(input("Довжина FG: "))
+g=int(input("Довжина GA: "))
+dict={"AB":0, "BC":0, "CD":0, "DE":0, "EF":0, "FG":0, "GA":0}
+dict["AB"]=a
+dict["BC"]=b
+dict["CD"]=c
+dict["DE"]=d
+dict["EF"]=e
+dict["FG"]=f
+dict["GA"]=g
+print(dict)
+print("Кількість сторін:",len(dict))
+print("Найбільший відрізок",max(dict.items(), key=operator.itemgetter(1)))
+x=input("Видалити відрізок: ")
+poppedItem = dict.pop(x)
+print("Сума усіх сторін",sum(dict.values()))
